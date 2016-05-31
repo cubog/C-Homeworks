fd988d5d23f006ac6c22bd014741307bcc4313a0
cubog <doug33193@gmail.com>
2016-05-31 14:47:00 -0400

    Homework 3 commit

---
+++ HW3/HW3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HW3.Startup))]
+namespace HW3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
