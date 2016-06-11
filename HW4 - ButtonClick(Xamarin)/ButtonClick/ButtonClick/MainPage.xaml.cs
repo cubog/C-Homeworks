97e5e2f1c860014c21f9a409b1e549bd94c10635
cubog <doug33193@gmail.com>
2016-06-11 13:40:44 -0400

    Second Part of HW4, Xamarin Forms app for Android and IOS

---
+++ HW4 - ButtonClick(Xamarin)/ButtonClick/ButtonClick/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ButtonClick
+{
+    public partial class MainPage : ContentPage
+    {
+        Random random;
+        String [] Phrases = { "You were born yourself, now help the others.",
+                    "There’s good climate in heaven, but a better company in hell.",
+                    "There are lots of good people, but much less of useful ones.",
+                    "Cleavage – is an art of being naked so that you’re still dressed up.",
+                    "Everyone has a conscience: it doesn’t matter if he wants it or not.",
+                    "There is no point of running away form a sniper. You will die from exhaustion."};
+        public MainPage()
+        {
+            InitializeComponent();
+            random = new Random();
+
+        }
+        void NewPhrase(object sender, EventArgs e)
+        {
+            
+            int randomNumber = random.Next(0, 6);
+            LabelText.Text = Phrases[randomNumber];
+        }
+    }
+}
